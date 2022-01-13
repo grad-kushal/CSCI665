1b7e683ed1c17bd7f1172cc374da6a958803686f
Kushal Kale <kushalkale@kushals-air.wireless.rit.edu>
2022-01-13 18:34:50 -0500

    Add 2nd solution

---
+++ HW0/src/Evensum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Evensum {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int number = scanner.nextInt();
+        if(number > -1){
+            for (int i = 0; i < number; i++){
+                int additionTerm = scanner.nextInt();
+                if(additionTerm % 2 == 0){
+                    sum += additionTerm;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
