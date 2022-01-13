111b34a7eebeb56b970b4fb00d19d31ba0f1a76e
Kushal Kale <kushalkale@kushals-air.wireless.rit.edu>
2022-01-13 18:34:18 -0500

    Add 2nd solution

--- HW0/src/Cubes.java
+++ HW0/src/Cubes.java
@@ -4,8 +4,10 @@ public class Cubes {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        for (int i =0; Math.pow(i, 3) < number; i++){
-            System.out.println(i*i*i);
+        if(number > -1){
+            for (int i =0; Math.pow(i, 3) < number; i++){
+                System.out.println(i*i*i);
+            }
         }
     }
 }
