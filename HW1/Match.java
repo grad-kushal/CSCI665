0fe7a5db9f143acdab87f463b50f20d1e8223ebc
grad-kushal <ksk7657@g.rit.edu>
2022-01-26 11:54:14 -0500

    Commit partial implementation

--- HW1/Match.java
+++ HW1/Match.java
@@ -1,23 +1,63 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Match {
     public static void main(String[] args) {
-        ArrayList<String> plist1 = new ArrayList<>();
-        ArrayList<String> plist2 = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> integerPrefList1 = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> integerPrefList2 = new ArrayList<>();
+        Stack<Integer> freeAskers = new Stack<>();
+
         try{
             Scanner scanner = new Scanner(System.in);
             int n = scanner.nextInt();
+
+            int [] currentPartners1 = new int[n];
+            int [] currentPartners2 = new int[n];
+
             for (int i = 0; i < 2*n; i++) {
+                ArrayList<Integer> tempPrefList = new ArrayList<>();
+                String temp = scanner.nextLine();
+                for (String p: temp.split(" ")) {
+                    tempPrefList.add(Integer.parseInt(p));
+                }
                 if (i < n){
-                    plist1.add(scanner.nextLine());
+
+                    integerPrefList1.add(tempPrefList);
                 }
                 else {
-                    plist2.add(scanner.nextLine());
+                    integerPrefList2.add(tempPrefList);
+                }
+            }
+
+            while (!freeAskers.isEmpty()){
+                int asker = freeAskers.pop();
+                ArrayList<Integer> askerPrefList = integerPrefList1.get(asker);
+                for (int responder : askerPrefList){
+                    if (!isAlreadyAsked(responder)) {
+                        if (getMatch(currentPartners2, responder) == 0){            //responder is free
+                            currentPartners1[asker] = responder;
+                            currentPartners2[responder] = asker;
+                        }
+                        else if (getMatch(currentPartners2, responder) !=0 &&  ){
+
+                        }
+                    }
                 }
             }
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public static int getMatch(int [] currentPartners, int i){
+        return currentPartners[i];
+    }
+
+    public static boolean isAlreadyAsked(int i){
+        if (i == -1)
+            return true;
+        else
+            return false;
     }
 }
