8aa16d974bedf7e631adf30f3754e1d8eb353602
grad-kushal <ksk7657@g.rit.edu>
2022-02-08 22:31:35 -0500

    Remove file to system in hack

--- HW2/src/Queens.java
+++ HW2/src/Queens.java
@@ -1,10 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Queens {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws Exception{
+//        FileInputStream fis = new FileInputStream(new File("/Users/kushalkale/Kushal/CSCI665/HW2/src/input.txt"));
+//        System.setIn(fis);
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.nextLine();
@@ -36,64 +35,6 @@ public class Queens {
             }
         }
         System.out.println(result == true ? "YES" : "NO");
-//        System.out.println(Arrays.toString(r));
-//        System.out.println(Arrays.toString(c));
-//        System.out.println(Arrays.toString(d1));
-//        System.out.println(Arrays.toString(d2));
-//        Board b = new Board(n);
-//        for (int i = 0; i < n; i++) {
-//            String [] queenPosition = scanner.nextLine().split(" ");
-//            boolean flag = b.placeQueen(Integer.parseInt(queenPosition[0]), Integer.parseInt(queenPosition[1]));
-//            if (flag == false) {
-//                break;
-//            }
-//            b.printBoard();
-//        }
-//        b.printBoard();
 
     }
 }
-
-//class Board {
-//    public Board(int size) {
-//        this.layout = new char[size][size];
-//        this.size = size;
-//    }
-//
-//    public int getSize() {
-//        return size;
-//    }
-//
-//    public void setSize(int size) {
-//        this.size = size;
-//    }
-//
-//    public boolean placeQueen(int x, int y) {
-//        int hIndex = x-1;
-//        int vIndex = y-1;
-//        boolean result = false;
-//        if (this.layout[hIndex][vIndex] != '.' && this.layout[x-1][y-1] != 'Q') {
-//            this.layout[hIndex][vIndex] = 'Q';
-//            result = true;
-//        }
-////        for (int i = 0; i < this.size; i++) {
-////            this.layout[hIndex][(vIndex + i + 1)%size] = '.';
-////            this.layout[(hIndex + i + 1)%size][vIndex] = '.';
-////            this.layout[(hIndex + i + 1)%size][(vIndex + i + 1)%size] = '.';
-////        }
-//        return result;
-//    }
-//
-//    int size;
-//    char [][] layout;
-//
-//    public void printBoard() {
-//        for (int i = 0; i < this.size; i++) {
-//            for (int j = 0; j < this.size; j++) {
-//                System.out.print("\t" + this.layout[i][j]);
-//            }
-//            System.out.println("");
-//        }
-//    }
-//}
-
